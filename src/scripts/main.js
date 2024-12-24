97c31069675cec8249367c0168d6a8ad398022cf
Maksym Korchynskyi <maksym.24.09.2003@gmail.com>
2024-12-24 11:07:31 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+function addMessage(messageText, isError = false) {
+  const message = document.createElement('div');
+
+  message.textContent = messageText;
+  message.classList.add('message', isError ? 'error-message' : null);
+
+  document.body.append(message);
+}
+
+const logo = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => resolve());
+});
+
+promise1.then(() => addMessage('Promise was resolved!'));
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(reject, 3_000);
+});
+
+promise2.catch(() => addMessage('Promise was rejected!', true));
